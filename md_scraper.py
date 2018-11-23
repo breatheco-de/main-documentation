370bd19deee2ae427a169c5839ac3e58f2d7953a
dogOfRiddles <gporhp@gmail.com>
2018-11-23 16:21:55 -0500

    scraperFinal

--- md_scraper.py
+++ md_scraper.py
@@ -1,10 +1,39 @@
 import requests
 import re
+from pathlib import Path
 
 def findLinksMore(textOfFile):
     url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', textOfFile)
     return url
 
-longText = requests.get('https://raw.githubusercontent.com/breatheco-de/main-documentation/master/README.md').text
-#print(longText)
-print(findLinksMore(longText))
+def md_scraper(fileToWork):
+    mdHandler = open(fileToWork)
+    urlList = []
+    for line in mdHandler:
+        #if len(findLinksMore(line)) > 3:
+        urlsFound = findLinksMore(line)
+        if len(urlsFound) != 0:
+            urlList.append(re.sub('[\>\)\"\'\?\[\]\*]','',str(urlsFound)))
+
+    print('\n**results of scraping',fileToWork+'**\n')
+
+    for element in urlList:
+        while element.find(',') != -1:
+            urlStringLong = element.split(",")
+            element = urlStringLong[0]
+
+        while element.find(" ") != -1:
+                urlStringLong = element.split(" ")
+                element = urlStringLong[0]
+
+        md_response = requests.get(str(element).strip())
+        if md_response.status_code == 200:
+            verboseResponse = '200 : OK'
+        else:
+            verboseResponse = str(md_response.status_code) + ' : Bad'
+        print(element,verboseResponse)
+
+pathlist = Path('.').glob('**/*.md')
+for path in pathlist:
+    pathStrings = str(path)
+    md_scraper(pathStrings)
