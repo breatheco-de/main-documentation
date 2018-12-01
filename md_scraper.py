0a4df8e6dc506a1f1d1d015cf3c2348c0fb7dd40
Alejandro Sanchez <aalejo@gmail.com>
2018-12-01 01:37:50 +0000

    fixed error in tests

--- md_scraper.py
+++ md_scraper.py
@@ -7,7 +7,7 @@ import re
 from pathlib import Path
 
 def findLinksMore(textOfFile):
-    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(\?:%[0-9a-fA-F][0-9a-fA-F]))+', textOfFile)
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', textOfFile)
     return url
 
 def md_scraper(fileToWork):
